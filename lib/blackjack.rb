fd6e3daecddbd4b95ccb187eb578aa384a4c095d
Francis Mendez <fmendez884@gmail.com>
2019-04-09 03:31:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,8 +32,10 @@ def hit?(total)
   prompt_user
   get_user_input
     if get_user_input == 's'
+      display_card_total(total)
     elsif get_user_input == 'h' 
       deal_card
+      display_card_total(total)
     else
       invalid_command
       prompt_user
